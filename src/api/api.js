582ac4f3200959a1b62f3c52ec57a9a00669fff4
kwakhyun <khyun2348@naver.com>
2022-11-01 03:05:58 +0900

    Feat: axios instance creation, API request code

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  config.headers.Accept = process.env.REACT_APP_ACCEPT;
+  config.headers.Authorization = process.env.REACT_APP_TOKEN;
+  return config;
+});
+
+export const getIssueList = async (page) => {
+  const { data } = await api.get(`issues?state=open&sort=comments&direction=desc&per_page=10&page=${page}`);
+  return data;
+};
+
+export const getIssueDetail = async (issueNumber) => {
+  const { data } = await api.get(`/issues/${issueNumber}`);
+  return data;
+};
