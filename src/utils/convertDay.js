804f27fc7f3a7a3ee0f09b7324c4a535402a652a
kwakhyun <khyun2348@naver.com>
2022-11-01 03:32:11 +0900

    Feat: date conversion function

---
+++ src/utils/convertDay.js
@@ -0,0 +1,8 @@
+const convertDay = createdAt => {
+  let newDate = new Date(createdAt).toLocaleDateString();
+  newDate = newDate.split('.');
+  newDate = `${newDate[0]}년 ${newDate[1]}월 ${newDate[2]}일`;
+  return newDate;
+};
+
+export default convertDay;
